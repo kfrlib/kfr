b429deb14a80eca643bed5a64320a9305b7ba717
d.levin256@gmail.com <d.levin256@gmail.com>
2016-07-19 10:49:28 +0300

    biquad example: set titles

--- examples/biquads.cpp
+++ examples/biquads.cpp
@@ -37,49 +37,49 @@ int main(int argc, char** argv)
                                        biquad_notch(0.4, 0.5) };
         output = biquad(bq, simpleimpulse());
     }
-    plot_save("biquad_notch", output, options);
+    plot_save("biquad_notch", output, options + ", title='Five Biquad Notch filters'");
 
     {
         biquad_params<double> bq[] = { biquad_lowpass(0.2, 0.9) };
         output                     = biquad(bq, simpleimpulse());
     }
-    plot_save("biquad_lowpass", output, options);
+    plot_save("biquad_lowpass", output, options + ", title='Biquad Low pass filter (0.2, 0.9)'");
 
     {
         biquad_params<double> bq[] = { biquad_highpass(0.3, 0.1) };
         output                     = biquad(bq, simpleimpulse());
     }
-    plot_save("biquad_highpass", output, options);
+    plot_save("biquad_highpass", output, options + ", title='Biquad High pass filter (0.3, 0.1)'");
 
     {
         biquad_params<double> bq[] = { biquad_peak(0.3, 0.5, +9.0) };
         output                     = biquad(bq, simpleimpulse());
     }
-    plot_save("biquad_peak", output, options);
+    plot_save("biquad_peak", output, options + ", title='Biquad Peak filter (0.2, 0.5, +9)'");
 
     {
         biquad_params<double> bq[] = { biquad_peak(0.3, 3.0, -2.0) };
         output                     = biquad(bq, simpleimpulse());
     }
-    plot_save("biquad_peak2", output, options);
+    plot_save("biquad_peak2", output, options + ", title='Biquad Peak filter (0.3, 3, -3)'");
 
     {
         biquad_params<double> bq[] = { biquad_lowshelf(0.3, -1.0) };
         output                     = biquad(bq, simpleimpulse());
     }
-    plot_save("biquad_lowshelf", output, options);
+    plot_save("biquad_lowshelf", output, options + ", title='Biquad low shelf filter (0.3, -1)'");
 
     {
         biquad_params<double> bq[] = { biquad_highshelf(0.3, +9.0) };
         output                     = biquad(bq, simpleimpulse());
     }
-    plot_save("biquad_highshelf", output, options);
+    plot_save("biquad_highshelf", output, options + ", title='Biquad high shelf filter (0.3, +9)'");
 
     {
         biquad_params<double> bq[] = { biquad_bandpass(0.25, 0.2) };
         output                     = biquad(bq, simpleimpulse());
     }
-    plot_save("biquad_bandpass", output, options);
+    plot_save("biquad_bandpass", output, options + ", title='Biquad band pass (0.25, 0.2)'");
 
     return 0;
 }
