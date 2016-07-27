ad6ead53783f714c528bd7bf7974260251b2585b
d.levin256@gmail.com <d.levin256@gmail.com>
2016-07-27 18:44:11 +0300

    Include intrinsic headers only if needed

--- include/kfr/base/intrinsics.h
+++ include/kfr/base/intrinsics.h
@@ -2,11 +2,13 @@
 
 #include "kfr.h"
 
-#ifdef CID_ARCH_X86
+#ifdef CID_ARCH_SSE2
 #include <immintrin.h>
 #ifdef KFR_OS_WIN
 #include <intrin.h>
 #endif
-#else
+#endif
+
+#ifdef CID_ARCH_NEON
 #include <arm_neon.h>
 #endif
