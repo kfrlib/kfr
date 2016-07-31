41167b7a5358555ec12789232bb7e585511e87cf
samuriddle@gmail.com <samuriddle@gmail.com>
2016-07-31 22:47:05 +0300

    step for make_range

--- include/kfr/cometa.hpp
+++ include/kfr/cometa.hpp
@@ -1767,15 +1767,23 @@ struct range
     using const_reference = const T&;
     using pointer         = T*;
     using const_pointer   = const T*;
+    using diff_type       = decltype(std::declval<T>() - std::declval<T>());
+
+    constexpr range(value_type begin, value_type end, diff_type step) noexcept : value_begin(begin),
+                                                                                 value_end(end),
+                                                                                 step(step)
+    {
+    }
 
     struct iterator
     {
-        T value;
+        value_type value;
+        diff_type step;
         const_reference operator*() const { return value; }
         const_pointer operator->() const { return &value; }
         iterator& operator++()
         {
-            ++value;
+            value += step;
             return *this;
         }
         iterator operator++(int)
@@ -1784,18 +1792,28 @@ struct range
             ++(*this);
             return copy;
         }
-        bool operator!=(const iterator& other) const { return value != other.value; }
+        bool operator!=(const iterator& other) const
+        {
+            return step > 0 ? value < other.value : value > other.value;
+        }
     };
-    T value_begin;
-    T value_end;
-    iterator begin() const { return iterator{ value_begin }; }
-    iterator end() const { return iterator{ value_end }; }
+    value_type value_begin;
+    value_type value_end;
+    diff_type step;
+    iterator begin() const { return iterator{ value_begin, step }; }
+    iterator end() const { return iterator{ value_end, step }; }
 };
 
-template <typename T1, typename T2>
-range<common_type<T1, T2>> make_range(T1 begin, T2 end)
+template <typename T>
+range<T> make_range(T begin, T end)
 {
-    return { begin, end };
+    return range<T>(begin, end, end > begin ? 1 : -1);
+}
+
+template <typename T, typename diff_type = decltype(std::declval<T>() - std::declval<T>())>
+range<T> make_range(T begin, T end, diff_type step)
+{
+    return range<T>(begin, end, step);
 }
 
 template <typename T>
