c3abea02bc810374174b55e05d2116a63bf3f44b
d.levin256@gmail.com <d.levin256@gmail.com>
2016-11-08 06:06:05 +0300

    kfr.h: Include CMT_OS_NAME in version string

--- include/kfr/base/kfr.h
+++ include/kfr/base/kfr.h
@@ -16,10 +16,12 @@
 
 #ifdef CMT_ARCH_X64
 #define KFR_VERSION_FULL                                                                                     \
-    "KFR " KFR_VERSION_STRING " " CMT_STRINGIFY(CMT_ARCH_NAME) " 64-bit (" CMT_COMPIER_NAME ")"
+    "KFR " KFR_VERSION_STRING " " CMT_STRINGIFY(CMT_ARCH_NAME) " 64-bit (" CMT_COMPIER_NAME "/" CMT_OS_NAME  \
+                                                               ")"
 #else
 #define KFR_VERSION_FULL                                                                                     \
-    "KFR " KFR_VERSION_STRING " " CMT_STRINGIFY(CMT_ARCH_NAME) " 32-bit (" CMT_COMPIER_NAME ")"
+    "KFR " KFR_VERSION_STRING " " CMT_STRINGIFY(CMT_ARCH_NAME) " 32-bit (" CMT_COMPIER_NAME "/" CMT_OS_NAME  \
+                                                               ")"
 #endif
 
 #ifdef __cplusplus
@@ -35,4 +37,5 @@ constexpr const char version_full[]   = KFR_VERSION_FULL;
 #endif
 
 #define KFR_INTRIN CMT_INTRIN
+#define KFR_FUNC CMT_FUNC
 #define KFR_SINTRIN CMT_INTRIN static
