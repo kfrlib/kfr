27cad6dd2b8530e95decf15fcdd249ea266f7462
d.levin256@gmail.com <d.levin256@gmail.com>
2018-11-13 01:27:53 +0300

    Fix GCC version for ARM

--- cmake/arm.cmake
+++ cmake/arm.cmake
@@ -11,7 +11,7 @@ set (CMAKE_CXX_COMPILER_WORKS TRUE)
 set (CMAKE_C_COMPILER_WORKS TRUE)
 
 set (ARM_ROOT "/usr/arm-linux-gnueabihf/include")
-set (GCC_VER 4.8.4)
+set (GCC_VER 5.4.0)
 set (SYS_PATHS "-isystem ${ARM_ROOT}/c++/${GCC_VER} -isystem ${ARM_ROOT}/c++/${GCC_VER}/backward -isystem ${ARM_ROOT}/c++/${GCC_VER}/arm-linux-gnueabihf -isystem ${ARM_ROOT}")
 
 set (ARM_COMMON_FLAGS "-target arm-linux-gnueabihf -mcpu=cortex-a15 -mfpu=neon-vfpv4 -mfloat-abi=hard -static")
