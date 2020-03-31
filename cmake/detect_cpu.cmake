2f5f81000daeaf8d90df82b3db815c6f6a6d8a88
Justin Giannone <jgiannone@mdsol.com>
2020-03-31 15:32:18 -0400

    Move additional CPU & Architecture Detection functionality into detect_cpu cmake Bunch of debugging

--- cmake/detect_cpu.cmake
+++ cmake/detect_cpu.cmake
@@ -1,4 +1,23 @@
-function(detect_cpu KFR_SOURCE_DIR)
+message(STATUS "C++ compiler: ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION} ${CMAKE_CXX_COMPILER}")
+message(STATUS "CMAKE_SYSTEM_PROCESSOR : ${CMAKE_SYSTEM_PROCESSOR}")
+
+if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+    set(CLANG TRUE)
+else ()
+    set(CLANG FALSE)
+endif ()
+message(STATUS "CLANG : ${CLANG}")
+
+if (CMAKE_SYSTEM_PROCESSOR MATCHES "(x86)|(X86)|(amd64)|(AMD64)")
+    set(X86 TRUE)
+else ()
+    set(X86 FALSE)
+endif ()
+message(STATUS "X86 : ${X86}")
+
+message(STATUS "MSVC : ${MSVC}")
+
+function(detect_cpu KFR_INCLUDE_DIR KFR_CMAKE_DIR)
 
     if (NOT CPU_ARCH)
         set(CPU_ARCH detect)
@@ -8,10 +27,11 @@ function(detect_cpu KFR_SOURCE_DIR)
         message(STATUS "Detecting native cpu...")
         try_run(
             RUN_RESULT COMPILE_RESULT "${CMAKE_CURRENT_BINARY_DIR}/tmpdir"
-            ${KFR_SOURCE_DIR}/cmake/detect_cpu.cpp
+            ${KFR_CMAKE_DIR}/detect_cpu.cpp
             CMAKE_FLAGS
-                "-DINCLUDE_DIRECTORIES=${KFR_SOURCE_DIR}/include"
-                -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON
+                "-DINCLUDE_DIRECTORIES=${KFR_INCLUDE_DIR}"
+                -DCMAKE_CXX_STANDARD=17
+                -DCMAKE_CXX_STANDARD_REQUIRED=ON
                 -DCMAKE_CXX_EXTENSIONS=ON
             COMPILE_OUTPUT_VARIABLE COMPILE_OUT
             RUN_OUTPUT_VARIABLE RUN_OUT)
