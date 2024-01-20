2d1ed775b8b0cc4f6a224266fc80d2c62b951e85
d.levin256@gmail.com <d.levin256@gmail.com>
2024-01-20 01:01:08 +0000

    ci/run.sh: work out of tree

--- ci/run.sh
+++ ci/run.sh
@@ -2,13 +2,14 @@
 set -e
 
 build_dir=$1
+src_dir=$(pwd)
 shift
 echo Preparing directories...
 rm -r $build_dir || true
 mkdir $build_dir
 pushd $build_dir
-echo Running cmake -GNinja -DENABLE_TESTS=ON "$@" ..
-cmake -GNinja -DENABLE_TESTS=ON "$@" ..
+echo Running cmake -GNinja -DENABLE_TESTS=ON "$@" $src_dir
+cmake -GNinja -DENABLE_TESTS=ON "$@" $src_dir
 echo Running ninja...
 ninja
 echo Running tests...
