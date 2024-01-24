f09c5c153b04962c2aaa208fd48d5a3bcd46988e
d.levin256@gmail.com <d.levin256@gmail.com>
2024-01-24 07:12:05 +0000

    Use LINKER prefix

--- cmake/link_as_whole.cmake
+++ cmake/link_as_whole.cmake
@@ -1,13 +1,11 @@
 
 function (link_as_whole TARGET TYPE LIBRARY)
     if (APPLE)
-        target_link_options(${TARGET} ${TYPE} -Wl,-force_load
-                            $<TARGET_FILE:${LIBRARY}>)
+        target_link_options(${TARGET} ${TYPE} "LINKER:-force_load,$<TARGET_FILE:${LIBRARY}>")
     elseif (WIN32)
         target_link_options(${TARGET} ${TYPE}
                             /WHOLEARCHIVE:$<TARGET_FILE:${LIBRARY}>)
     else ()
-        target_link_options(${TARGET} ${TYPE} -Wl,--push-state,--whole-archive
-                            $<TARGET_FILE:${LIBRARY}> -Wl,--pop-state)
+        target_link_options(${TARGET} ${TYPE} "LINKER:--push-state,--whole-archive,$<TARGET_FILE:${LIBRARY}>,--pop-state")
     endif ()
 endfunction ()
