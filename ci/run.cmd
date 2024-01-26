2f7a006b2036ad036cec9430ca4870b25e0af4e3
d.levin256@gmail.com <d.levin256@gmail.com>
2024-01-26 13:40:20 +0000

    Set CMAKE_INSTALL_PREFIX in ci/run

--- ci/run.cmd
+++ ci/run.cmd
@@ -5,9 +5,10 @@ rmdir /s /q %build_dir%
 mkdir %build_dir%
 pushd %build_dir%
 echo Running cmake -GNinja -DENABLE_TESTS=ON %* ..
-cmake -GNinja -DENABLE_TESTS=ON %* .. || exit /b
+cmake -GNinja -DENABLE_TESTS=ON -DCMAKE_INSTALL_PREFIX=dist %* .. || exit /b
 echo Running ninja...
 ninja %JOBS% || exit /b
+ninja install || exit /b
 echo Running tests...
 cd tests && ctest -V || exit /b
 popd
--- ci/run.sh
+++ ci/run.sh
@@ -9,9 +9,10 @@ rm -r $build_dir || true
 mkdir $build_dir
 pushd $build_dir
 echo Running cmake -GNinja -DENABLE_TESTS=ON "$@" $src_dir
-cmake -GNinja -DENABLE_TESTS=ON "$@" $src_dir
+cmake -GNinja -DENABLE_TESTS=ON -DCMAKE_INSTALL_PREFIX=dist "$@" $src_dir
 echo Running ninja...
 ninja
+ninja install
 echo Running tests...
 cd tests && ctest -V
 popd
