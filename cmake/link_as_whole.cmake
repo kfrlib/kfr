f552aab362dfc4f861cd4de4b06bebdc41581c8c
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-04-28 14:07:57 -0400

    Another Win32 CMake portability fix
    GNU ld, etc. don't know about /WHOLEARCHIVE

--- cmake/link_as_whole.cmake
+++ cmake/link_as_whole.cmake
@@ -2,7 +2,7 @@
 function (link_as_whole TARGET TYPE LIBRARY)
     if (APPLE)
         target_link_options(${TARGET} ${TYPE} "LINKER:-force_load,$<TARGET_FILE:${LIBRARY}>")
-    elseif (WIN32)
+    elseif (MSVC)
         target_link_options(${TARGET} ${TYPE}
                             /WHOLEARCHIVE:$<TARGET_FILE:${LIBRARY}>)
     else ()
