07314aa510d70fff8a5f92f96d9000e8c70c689e
d.levin256@gmail.com <d.levin256@gmail.com>
2025-09-24 11:40:01 +0200

    Refactor add_kfr_library.cmake

--- cmake/add_kfr_library.cmake
+++ cmake/add_kfr_library.cmake
@@ -1,7 +1,11 @@
 function (add_kfr_library)
 
-    cmake_parse_arguments(LIB "MULTIARCH" "NAME" "SOURCES;LIBRARIES;DEFINITIONS;OPTIONS"
-                          ${ARGN})
+    cmake_parse_arguments(
+        LIB
+        "MULTIARCH"
+        "NAME"
+        "SOURCES;LIBRARIES;PUBLIC_LIBRARIES;DEFINITIONS;PUBLIC_DEFINITIONS;INCLUDE_DIRECTORIES;OPTIONS"
+        ${ARGN})
 
     set(${LIB_NAME}_LIBS)
     set(${LIB_NAME}_TARGETS)
@@ -9,21 +13,24 @@ function (add_kfr_library)
         add_library(${LIB_NAME} INTERFACE)
         foreach (ARCH IN LISTS KFR_ARCHS)
             add_library(${LIB_NAME}_${ARCH} STATIC ${LIB_SOURCES})
-            target_compile_definitions(${LIB_NAME}_${ARCH} PRIVATE KFR_MULTI=1)     
+            target_compile_definitions(${LIB_NAME}_${ARCH} PRIVATE KFR_MULTI=1)
             foreach (ENABLED_ARCH IN LISTS KFR_ARCHS)
                 string(TOUPPER ${ENABLED_ARCH} ENABLED_ARCH_UPPER)
-                target_compile_definitions(${LIB_NAME}_${ARCH} PRIVATE KFR_MULTI_ENABLED_${ENABLED_ARCH_UPPER}=1)   
-            endforeach()
+                target_compile_definitions(
+                    ${LIB_NAME}_${ARCH}
+                    PRIVATE KFR_MULTI_ENABLED_${ENABLED_ARCH_UPPER}=1)
+            endforeach ()
             list(APPEND ${LIB_NAME}_LIBS ${LIB_NAME}_${ARCH})
             list(APPEND ${LIB_NAME}_TARGETS ${LIB_NAME}_${ARCH})
             target_link_libraries(${LIB_NAME} INTERFACE ${LIB_NAME}_${ARCH})
             target_set_arch(${LIB_NAME}_${ARCH} PRIVATE ${ARCH})
         endforeach ()
         list(GET KFR_ARCHS 0 BASE_ARCH)
-        target_compile_definitions(${LIB_NAME}_${BASE_ARCH} PRIVATE KFR_BASE_ARCH=1)
+        target_compile_definitions(${LIB_NAME}_${BASE_ARCH}
+                                   PRIVATE KFR_BASE_ARCH=1)
 
         link_as_whole(${LIB_NAME} INTERFACE ${LIB_NAME}_${BASE_ARCH})
-        
+
         list(APPEND ${LIB_NAME}_TARGETS ${LIB_NAME})
     else ()
         add_library(${LIB_NAME} STATIC ${LIB_SOURCES})
@@ -35,11 +42,18 @@ function (add_kfr_library)
     foreach (LIB IN LISTS ${LIB_NAME}_LIBS)
         target_link_libraries(${LIB} PUBLIC kfr)
         target_link_libraries(${LIB} PRIVATE ${LIB_LIBRARIES})
+        target_link_libraries(${LIB} PUBLIC ${LIB_PUBLIC_LIBRARIES})
         target_compile_definitions(${LIB} PRIVATE ${LIB_DEFINITIONS})
-        target_compile_options(${LIB} PRIVATE ${LIB_OPTIONS})        
+        target_compile_definitions(${LIB} PUBLIC ${LIB_PUBLIC_DEFINITIONS})
+        target_compile_options(${LIB} PRIVATE ${LIB_OPTIONS})
+        target_include_directories(${LIB} PRIVATE ${LIB_INCLUDE_DIRECTORIES})
     endforeach ()
 
-    set(${LIB_NAME}_LIBS ${${LIB_NAME}_LIBS} PARENT_SCOPE)
-    set(${LIB_NAME}_TARGETS ${${LIB_NAME}_TARGETS} PARENT_SCOPE)
+    set(${LIB_NAME}_LIBS
+        ${${LIB_NAME}_LIBS}
+        PARENT_SCOPE)
+    set(${LIB_NAME}_TARGETS
+        ${${LIB_NAME}_TARGETS}
+        PARENT_SCOPE)
 
 endfunction ()
