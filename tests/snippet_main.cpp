e1dd4b084678a4a13aadf6a7440ead85292a999f
d.levin256@gmail.com <d.levin256@gmail.com>
2026-08-17 02:55:53 +0200

    Use PRIVATE for target_link_libraries; add snippet_main.cpp

---
+++ tests/snippet_main.cpp
@@ -0,0 +1,20 @@
+/*
+ * KFR (https://www.kfrlib.com)
+ * Copyright (C) 2016-2026 Dan Casarin
+ * See LICENSE.txt for details
+ */
+
+#include <kfr/io/tostring.hpp>
+#include <kfr/test/test.hpp>
+#include <kfr/version.hpp>
+
+using namespace kfr;
+
+int main(int argc, char* argv[])
+{
+    println(library_version(), " running on ", cpu_runtime());
+
+    int result = Catch::Session().run(argc, argv);
+
+    return result;
+}
